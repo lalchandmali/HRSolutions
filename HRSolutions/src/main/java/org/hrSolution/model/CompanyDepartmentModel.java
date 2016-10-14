16d2ccc2ced2672a6d9a7f23257acd6bf5232079
Abhimanyu <abhimanyubaranwal@le.com>
2016-10-14 14:49:09 +0530

    Comapny Department Model and Company Role Model CLASS modified with comments

--- HRSolutions/src/main/java/org/hrSolution/model/CompanyDepartmentModel.java
+++ HRSolutions/src/main/java/org/hrSolution/model/CompanyDepartmentModel.java
@@ -2,68 +2,119 @@ package org.hrSolution.model;
 
 import org.codehaus.jackson.annotate.JsonProperty;
 
+/**
+ *  This class used for getter setter of Company Department with having
+ *  department Name and ID
+ *  We have used Builder Design Pattern for setting up variables
+ *  
+ *  @author  Abhimanyu Baranwal
+ *  @since   14-10-2016 
+ */
 
 public class CompanyDepartmentModel {
 
-	@JsonProperty("compName")
-	private String companyName;				// Company Name
-	
-	@JsonProperty("compDeptId")
-	private String companyDepartmentId;		// Company Department ID
-	
-	@JsonProperty("compDeptName")
-	private String companyDepartmentName;	// Company Department Name
+	/**
+	 * Company Name
+	 */
+	@JsonProperty("cName")
+	private String companyName;
 
-	public CompanyDepartmentModel (CompanyDepartmentModelBuilder companydeptmodelbuilder )
-	{
-		this.companyName= companydeptmodelbuilder.companyName;
-		this.companyName= companydeptmodelbuilder.companyName;
-		this.companyName= companydeptmodelbuilder.companyName;
+	/**
+	 * Company Department ID
+	 */
+	@JsonProperty("cDId")
+	private String companyDepartmentId;
+
+	/**
+	 * Company Department Name
+	 */
+	@JsonProperty("cDName")
+	private String companyDepartmentName;
+
+	/**
+	 * Parameterized Constructor
+	 */
+	public CompanyDepartmentModel(CompanyDepartmentModelBuilder companyDeptModelbuilder) {
+		this.companyName = companyDeptModelbuilder.companyName;
+		this.companyDepartmentName = companyDeptModelbuilder.companyDepartmentName;
+		this.companyDepartmentId = companyDeptModelbuilder.companyDepartmentId;
 	}
-	
-	
+
+	/**
+	 * Getting Company Department ID
+	 */
 	public String getCompanyDepartmentId() {
 		return companyDepartmentId;
 	}
 
+	/**
+	 * Getting Company Department NAME
+	 */
 	public String getCompanyDepartmentName() {
 		return companyDepartmentName;
 	}
 
+	/**
+	 * Getting Company NAME
+	 */
 	public String getCompanyName() {
 		return companyName;
 	}
-
+	
+	/**
+	 * Builder class for CompanyRoleModel
+	 */
 	public static class CompanyDepartmentModelBuilder {
 
-		@JsonProperty("compName")
+		/**
+		 * Company Name
+		 */
+		@JsonProperty("cName")
 		private String companyName;
-		
-		@JsonProperty("compDeptId")
+
+		/**
+		 * Company Department ID
+		 */
+		@JsonProperty("cDId")
 		private String companyDepartmentId;
-		
-		@JsonProperty("compDeptName")
+
+		/**
+		 * Company Department Name
+		 */
+		@JsonProperty("cDName")
 		private String companyDepartmentName;
 
+		/**
+		 * Setting Company NAME
+		 */
 		public CompanyDepartmentModelBuilder companyName(String companyName) {
 			this.companyName = companyName;
 			return this;
 		}
 
+		/**
+		 * Setting Company Department ID
+		 */
 		public CompanyDepartmentModelBuilder companyDepartmentId(String companyDepartmentId) {
 			this.companyDepartmentId = companyDepartmentId;
 			return this;
 		}
 
-		public CompanyDepartmentModelBuilder companyDepartmentName(String preEmpPhone) {
+		/**
+		 * Setting Company Department NAME
+		 */
+		public CompanyDepartmentModelBuilder companyDepartmentName(String companyDepartmentName) {
 			this.companyDepartmentName = companyDepartmentName;
 			return this;
 		}
 
+		/**
+		 * Build Method
+		 */
 		public CompanyDepartmentModel build() {
 			return new CompanyDepartmentModel(this);
 		}
 
 	}
-	
+
 }
--- HRSolutions/src/main/java/org/hrSolution/model/CompanyRoleModel.java
+++ HRSolutions/src/main/java/org/hrSolution/model/CompanyRoleModel.java
@@ -2,69 +2,118 @@ package org.hrSolution.model;
 
 import org.codehaus.jackson.annotate.JsonProperty;
 
+/**
+ *  This class used for getter setter of Company Role with access
+ *  rights of Each Department.
+ *  We have used Builder Design Pattern for setting up variables
+ *  
+ *  @author  Abhimanyu Baranwal
+ *  @since   14-10-2016 
+ */
+
 public class CompanyRoleModel {
+	
+	/**
+	 * Company Department ID
+	 */
+	@JsonProperty("cDId")
+	private String companyDepartmentId;
 
-	
-	@JsonProperty("compDeptId")
-	private String companyDepartmentId;		// Company Department ID
-	
-	@JsonProperty("compDeptName")
-	private String companyDepartmentName;	// Company Departname NAME
-	
-	@JsonProperty("deptAccessRights")
-	private String departmentAccessRights;	// Department Access Rights
+	/**
+	 * Company Department NAME
+	 */
+	@JsonProperty("cDName")
+	private String companyDepartmentName;
 
+	/**
+	 * Department Access Rights
+	 */
+	@JsonProperty("dARights")
+	private String departmentAccessRights;
+
+	/**
+	 * Parameterized Constructor
+	 */
 	public CompanyRoleModel(CompanyRoleModelBuilder companyRoleModelBuilder) {
-		this.companyDepartmentId=companyRoleModelBuilder.companyDepartmentId;
-		this.companyDepartmentName=companyRoleModelBuilder.companyDepartmentName;
-		this.departmentAccessRights=companyRoleModelBuilder.departmentAccessRights;
+		this.companyDepartmentId = companyRoleModelBuilder.companyDepartmentId;
+		this.companyDepartmentName = companyRoleModelBuilder.companyDepartmentName;
+		this.departmentAccessRights = companyRoleModelBuilder.departmentAccessRights;
 	}
 
+	/**
+	 * Getting Company Department ID
+	 */
 	public String getCompanyDepartmentId() {
 		return companyDepartmentId;
 	}
 
+	/**
+	 * Getting Company Department NAME
+	 */
 	public String getCompanyDepartmentName() {
 		return companyDepartmentName;
 	}
 
+	/**
+	 * Getting Department Access Rights
+	 */
 	public String getDepartmentAccessRights() {
 		return departmentAccessRights;
 	}
-	
-	
-	// Builder class of CompanyRoleModel
+
+	/**
+	 * Builder class for CompanyRoleModel
+	 */
 	public static class CompanyRoleModelBuilder {
-		
-		@JsonProperty("compDeptId")
+
+		/**
+		 * Company Department ID
+		 */
+		@JsonProperty("cDId")
 		private String companyDepartmentId;
-		
-		@JsonProperty("compDeptName")
+
+		/**
+		 * Company Department NAME
+		 */
+		@JsonProperty("cDName")
 		private String companyDepartmentName;
-		
-		@JsonProperty("deptAccessRights")
+
+		/**
+		 * Department Access Rights
+		 */
+		@JsonProperty("dARights")
 		private String departmentAccessRights;
-		
+
+		/**
+		 * Setting Company Department ID
+		 */
 		public CompanyRoleModelBuilder setCompanyDeptId(String companyDepartmentId) {
-			this.companyDepartmentId=companyDepartmentId;
+			this.companyDepartmentId = companyDepartmentId;
 			return this;
 		}
-		
+
+		/**
+		 * Setting Company Department NAME
+		 */
 		public CompanyRoleModelBuilder setCompanyDeptName(String companyDepartmentName) {
-			this.companyDepartmentName=companyDepartmentName;
+			this.companyDepartmentName = companyDepartmentName;
 			return this;
 		}
-		
-		public CompanyRoleModelBuilder setDeptAccessRight(String departmentAccessRights)
-		{
-			this.departmentAccessRights=departmentAccessRights;
+
+		/**
+		 * Setting Department Access Rights
+		 */
+		public CompanyRoleModelBuilder setDeptAccessRight(String departmentAccessRights) {
+			this.departmentAccessRights = departmentAccessRights;
 			return this;
 		}
-		
-		public CompanyRoleModel build()
-		{
+
+		/**
+		 * Build Method
+		 */
+		public CompanyRoleModel build() {
 			return new CompanyRoleModel(this);
 		}
 	}
-	
+
 }
