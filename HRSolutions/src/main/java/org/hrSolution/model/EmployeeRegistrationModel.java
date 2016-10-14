5fde070308cd95971c45800241b3a3cc94befdde
lalchandmali <lalchandmali92@gmail.com>
2016-10-14 13:18:55 +0530

    adding comment

--- HRSolutions/src/main/java/org/hrSolution/model/EmployeeRegistrationModel.java
+++ HRSolutions/src/main/java/org/hrSolution/model/EmployeeRegistrationModel.java
@@ -2,7 +2,8 @@ package org.hrSolution.model;
 
 import org.codehaus.jackson.annotate.JsonProperty;
 
-/*This model class is used for setting and getting variable for
+/**
+ * This model class is used for setting and getting variable for
  * Employee means Employee is already member of company
  * */
 /**
@@ -11,51 +12,53 @@ import org.codehaus.jackson.annotate.JsonProperty;
  */
 public class EmployeeRegistrationModel {
 
-	/* class attributes declarations */
-	/*
+	/**
+	 * class attributes declarations
+	 */
+	/**
 	 * Employee Id variable declaration
 	 */
 	@JsonProperty("eid")
 	private String employeeId;
 
-	/*
+	/**
 	 * Employee name variable declaration
 	 */
 	@JsonProperty("ename")
 	private String employeeName;
 
-	/*
+	/**
 	 * Employee roleId (e.g. HR, Senior Manager,Manager,Lead) variable
 	 * declaration
 	 */
 	@JsonProperty("rid")
 	private Integer roleId;
 
-	/*
+	/**
 	 * Employee department Id variable declaration
 	 */
 	@JsonProperty("did")
 	private Integer departmentId;
 
-	/*
+	/**
 	 * Employee Company Id variable declaration
 	 */
 	@JsonProperty("cid")
 	private String companyId;
 
-	/*
+	/**
 	 * Employee Mail Id variable declaration
 	 */
 	@JsonProperty("mid")
 	private String mailId;
 
-	/*
+	/**
 	 * Employee Password variable declaration
 	 */
 	@JsonProperty("pwd")
 	private String password;
 
-	/* Parameterized constructor for setting attributes values */
+	/** Parameterized constructor for setting attributes values */
 	public EmployeeRegistrationModel(EmployeeRegistrationBuilder employeeRegistrationBuilder) {
 
 		this.employeeId = employeeRegistrationBuilder.employeeId;
@@ -67,102 +70,104 @@ public class EmployeeRegistrationModel {
 		this.password = employeeRegistrationBuilder.password;
 	}
 
-	/* Getting Employee Id */
+	/** Getting Employee Id */
 	public String getEmployeeId() {
 		return employeeId;
 	}
 
-	/* Getting Employee name */
+	/** Getting Employee name */
 	public String getEmployeeName() {
 		return employeeName;
 	}
 
-	/*
+	/**
 	 * Getting Role Id HR or Senior manager or manager or Lead or Head
 	 */
 	public Integer getRoleId() {
 		return roleId;
 	}
 
-	/* Getting department Id */
+	/** Getting department Id */
 	public Integer getDepartmentId() {
 		return departmentId;
 	}
 
-	/* Getting company Id */
+	/** Getting company Id */
 	public String getCompanyId() {
 		return companyId;
 	}
 
-	/* Getting mail Id */
+	/** Getting mail Id */
 	public String getMailId() {
 		return mailId;
 	}
 
-	/* Getting password */
+	/** Getting password */
 	public String getPassword() {
 		return password;
 	}
 
 	/* EmployeeRegistrationBuilder class for building the result */
 	public static class EmployeeRegistrationBuilder {
-		/* class attributes declarations */
-		/*
+		/**
+		 * class attributes declarations
+		 */
+		/**
 		 * Employee Id variable declaration
 		 */
 		@JsonProperty("eid")
 		private String employeeId;
 
-		/*
+		/**
 		 * Employee name variable declaration
 		 */
 		@JsonProperty("ename")
 		private String employeeName;
 
-		/*
+		/**
 		 * Employee roleId (e.g. HR, Senior Manager,Manager,Lead) variable
 		 * declaration
 		 */
 		@JsonProperty("rid")
 		private Integer roleId;
 
-		/*
+		/**
 		 * Employee department Id variable declaration
 		 */
 		@JsonProperty("did")
 		private Integer departmentId;
 
-		/*
+		/**
 		 * Employee Company Id variable declaration
 		 */
 		@JsonProperty("cid")
 		private String companyId;
 
-		/*
+		/**
 		 * Employee Mail Id variable declaration
 		 */
 		@JsonProperty("mid")
 		private String mailId;
 
-		/*
+		/**
 		 * Employee Password variable declaration
 		 */
 		@JsonProperty("pwd")
 		private String password;
 
-		/* Setting Employee name by Builder */
+		/** Setting Employee name by Builder */
 		public EmployeeRegistrationBuilder empName(String employeeName) {
 			this.employeeName = employeeName;
 			return this;
 		}
 
-		/* Setting Employee mail by Builder */
+		/** Setting Employee mail by Builder */
 		public EmployeeRegistrationBuilder employeeId(String employeeId) {
 			this.employeeId = employeeId;
 			return this;
 		}
 
-		/* Setting Employee mail by Builder */
+		/** Setting Employee mail by Builder */
 		public EmployeeRegistrationBuilder mailId(String mailId) {
 			this.mailId = mailId;
 			return this;
@@ -174,19 +179,19 @@ public class EmployeeRegistrationModel {
 			return this;
 		}
 
-		/* Setting Employee departmentId by Builder */
+		/** Setting Employee departmentId by Builder */
 		public EmployeeRegistrationBuilder departmentId(Integer departmentId) {
 			this.departmentId = departmentId;
 			return this;
 		}
 
-		/* Setting Employee password by Builder */
+		/** Setting Employee password by Builder */
 		public EmployeeRegistrationBuilder password(String password) {
 			this.password = password;
 			return this;
 		}
 
-		/* Build method */
+		/** Build method */
 		public EmployeeRegistrationModel build() {
 			return new EmployeeRegistrationModel(this);
 		}
--- HRSolutions/src/main/java/org/hrSolution/model/PreEmployeeRegistrationModel.java
+++ HRSolutions/src/main/java/org/hrSolution/model/PreEmployeeRegistrationModel.java
@@ -2,7 +2,7 @@ package org.hrSolution.model;
 
 import org.codehaus.jackson.annotate.JsonProperty;
 
-/*This model class is used for setting and getting variable for
+/** This model class is used for setting and getting variable for
  * preEmployment means Employee has not joined the company
  * */
 /**
@@ -10,35 +10,35 @@ import org.codehaus.jackson.annotate.JsonProperty;
  *
  */
 public class PreEmployeeRegistrationModel {
-	/*
+	/**
 	 * class attributes declaration
 	 */
 
-	/*
+	/**
 	 * preEmployee name variable declaration
 	 */
 	@JsonProperty("pname")
 	private String preEmpName;
 
-	/*
+	/**
 	 * preEmployee Mail variable declaration
 	 */
 	@JsonProperty("pmail")
 	private String preEmpMail;
 
-	/*
+	/**
 	 * preEmployee Phone variable declaration
 	 */
 	@JsonProperty("pphone")
 	private String preEmpPhone;
 
-	/*
+	/**
 	 * preEmployee Password variable declaration
 	 */
 	@JsonProperty("ppass")
 	private String preEmpPassword;
 
-	/* Parameterized constructor for setting attributes values */
+	/** Parameterized constructor for setting attributes values */
 	public PreEmployeeRegistrationModel(PreEmployeeRegistrationBuilder preEmployeeRegistrationBuilder) {
 		this.preEmpName = preEmployeeRegistrationBuilder.preEmpName;
 		this.preEmpMail = preEmployeeRegistrationBuilder.preEmpMail;
@@ -46,81 +46,81 @@ public class PreEmployeeRegistrationModel {
 		this.preEmpPassword = preEmployeeRegistrationBuilder.preEmpPassword;
 	}
 
-	/* Getting Employee name */
+	/** Getting Employee name */
 	public String getPreEmpName() {
 		return preEmpName;
 	}
 
-	/* Getting Employee mail */
+	/** Getting Employee mail */
 	public String getPreEmpMail() {
 		return preEmpMail;
 	}
 
-	/* Getting Employee Phone number */
+	/** Getting Employee Phone number */
 	public String getPreEmpPhone() {
 		return preEmpPhone;
 	}
 
-	/* Getting Employee Password */
+	/** Getting Employee Password */
 	public String getPreEmpPassword() {
 		return preEmpPassword;
 	}
 
 	/* EmployeeRegistrationBuilder class for building the result */
 	public static class PreEmployeeRegistrationBuilder {
-		/*
+		/**
 		 * class attributes declaration
 		 */
 
-		/*
+		/**
 		 * preEmployee name variable declaration
 		 */
 		@JsonProperty("pname")
 		private String preEmpName;
 
-		/*
+		/**
 		 * preEmployee Mail variable declaration
 		 */
 		@JsonProperty("pmail")
 		private String preEmpMail;
 
-		/*
+		/**
 		 * preEmployee Phone variable declaration
 		 */
 		@JsonProperty("pphone")
 		private String preEmpPhone;
 
-		/*
+		/**
 		 * preEmployee Password variable declaration
 		 */
 		@JsonProperty("ppass")
 		private String preEmpPassword;
 
-		/* Setting Employee name by Builder */
+		/** Setting Employee name by Builder */
 		public PreEmployeeRegistrationBuilder preEmpName(String preEmpName) {
 			this.preEmpName = preEmpName;
 			return this;
 		}
 
-		/* Setting Employee mail by Builder */
+		/** Setting Employee mail by Builder */
 		public PreEmployeeRegistrationBuilder preEmpMail(String preEmpMail) {
 			this.preEmpMail = preEmpMail;
 			return this;
 		}
 
-		/* Setting Employee Phone by Builder */
+		/** Setting Employee Phone by Builder */
 		public PreEmployeeRegistrationBuilder preEmpPhone(String preEmpPhone) {
 			this.preEmpPhone = preEmpPhone;
 			return this;
 		}
 
-		/* Setting Employee password by Builder */
+		/** Setting Employee password by Builder */
 		public PreEmployeeRegistrationBuilder preEmpPassword(String preEmpPassword) {
 			this.preEmpPassword = preEmpPassword;
 			return this;
 		}
 
-		/* Build method */
+		/** Build method */
 		public PreEmployeeRegistrationModel build() {
 			return new PreEmployeeRegistrationModel(this);
 		}
